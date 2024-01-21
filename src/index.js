9da38610d1ca330e80df50c0b29e607b8416dc8f
Alexandros Collie <amcollie@gmail.com>
2024-01-21 12:45:24 -0500

    get request

--- src/index.js
+++ src/index.js
@@ -1,7 +1,44 @@
 import express from 'express'
 
 const app = express()
+
 const PORT = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/api/users', (req, res) => {
+    res.send([
+        {
+            id: 1,
+            username: 'john_doe',
+            displayName: 'John Doe',
+        },
+        {
+            id: 2,
+            username: 'jane_doe',
+            displayName: 'Jane Doe',
+        },
+        {
+            id: 3,
+            username: 'john_smith',
+            displayName: 'John Smith',
+        }
+    ])
+})
+
+app.get('/api/products', (req, res) => {
+    res.send([
+        {
+            id: 123,
+            name: 'chicken breast',
+            price: 12.99,
+        },
+    ])
+})
+
+
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
